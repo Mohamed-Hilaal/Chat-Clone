d5d65f212299636341018852e432a82b0891be68
Mohamed Hilaal <87385838+Mohamed-Hilaal@users.noreply.github.com>
2021-12-10 07:52:27 +0530

    Add files via upload

---
+++ Chatt/build/precache-manifest.da42bebec9d09b2c8aab31d4a0b0fd78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f090cf7db79d09cc02b93f2b7ed940b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c305d0599257b3727573",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c305d0599257b3727573",
+    "url": "/static/js/2.4185bfc8.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.4185bfc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35e29242fe6001335f1",
+    "url": "/static/js/main.e6d97893.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
